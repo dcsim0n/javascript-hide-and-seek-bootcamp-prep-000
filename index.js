bf4a91f3378647cbd8b0c96f5a94b71bb5a4b0f3
dsanalog <dsanalog@gmail.com>
2019-01-04 17:56:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  //Do some work Newton!
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+  //More work Newton!
+}
+
+function increaseRankBy(inc){
+ var rankedList = document.querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < rankedList.length; i++){
+   var rank = parseInt(rankedList[i].innerHTML);
+   console.log(rankedList[i].innerHTML);
+   rank = rank + inc;
+   rankedList[i].innerHTML = rank.toString();
+ }
+}
+
+
+function deepestChild(){
+  
+  return child;
+}
